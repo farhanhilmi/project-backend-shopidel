2524c1e859c40058d4cb1e87187d7e7586d27c6e
Muhammad Acla Alamsyah Nurdin Hamzah <acla.hamzah@shopee.com>
2023-11-01 12:54:34 +0700

    fix: remove json tag in dto usecase & repository

--- dto/usecase/account.go
+++ dto/usecase/account.go
@@ -20,24 +20,24 @@ type CreateAccountResponse struct {
 }
 
 type GetAccountRequest struct {
-	UserId int `json:"id"`
+	UserId int 
 }
 
 type GetAccountResponse struct {
-	ID                      int             `json:"id,omitempty"`
-	FullName                string          `json:"full_name,omitempty"`
-	Username                string          `json:"username,omitempty"`
-	Email                   string          `json:"email,omitempty"`
-	PhoneNumber             string          `json:"phone_number,omitempty"`
-	ShopName                string          `json:"shop_name,omitempty"`
-	Gender                  string          `json:"gender,omitempty"`
-	Birthdate               time.Time       `json:"birthdate,omitempty"`
-	ProfilePicture          string          `json:"profile_picture,omitempty"`
-	WalletNumber            string          `json:"wallet_number,omitempty"`
-	WalletPin               string          `json:"wallet_pin,omitempty"`
-	Balance                 decimal.Decimal `json:"balance,omitempty"`
-	ForgetPasswordToken     string          `json:"forget_password_token,omitempty"`
-	ForgetPasswordExpiredAt time.Time       `json:"forget_password_expired_at,omitempty"`
+	ID                      int             
+	FullName                string          
+	Username                string          
+	Email                   string          
+	PhoneNumber             string          
+	ShopName                string          
+	Gender                  string          
+	Birthdate               time.Time       
+	ProfilePicture          string          
+	WalletNumber            string          
+	WalletPin               string          
+	Balance                 decimal.Decimal 
+	ForgetPasswordToken     string          
+	ForgetPasswordExpiredAt time.Time       
 }
 
 
