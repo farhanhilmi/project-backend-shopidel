d6cb58caac5b7c294a48babba47dc71e1203f2ca
Farhan Hilmi <farhan.hilmi@shopee.com>
2023-11-15 10:27:18 +0700

    feat(be-126): upload photo to cloudinary

--- server/http/handler/product_handler.go
+++ server/http/handler/product_handler.go
@@ -2,8 +2,12 @@ package handler
 
 import (
 	"errors"
+	"fmt"
+	"log"
 	"net/http"
+	"path"
 	"strconv"
+	"time"
 
 	dtogeneral "git.garena.com/sea-labs-id/bootcamp/batch-01/group-project/pejuang-rupiah/backend/dto/general"
 	dtousecase "git.garena.com/sea-labs-id/bootcamp/batch-01/group-project/pejuang-rupiah/backend/dto/usecase"
@@ -21,6 +25,33 @@ func NewProductHandler(productUsecase usecase.ProductUsecase) *ProductHandler {
 	return &ProductHandler{
 		productUsecase: productUsecase,
 	}
+}
+
+func (h *ProductHandler) UploadPhotos(c *gin.Context) {
+	file, header, err := c.Request.FormFile("file[]")
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "Bad request",
+		})
+		return
+	}
+	log.Println("FILE", file)
+	return
+
+	currentTime := time.Now().UnixNano()
+	fileExtension := path.Ext(header.Filename)
+
+	originalFilename := header.Filename[:len(header.Filename)-len(fileExtension)]
+
+	newFilename := fmt.Sprintf("%s_%d", originalFilename, currentTime)
+
+	imageUrl, err := util.UploadToCloudinary(file, newFilename)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, dtogeneral.JSONResponse{Data: imageUrl})
 }
 
 func (h *ProductHandler) ListProduct(c *gin.Context) {
